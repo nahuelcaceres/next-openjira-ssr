4100c032077f53e1dc61b88625b13ebeda68a5c6
Nahuel <ncaceres@andreani.com>
2023-04-02 18:53:30 -0300

    fix: import uiReducer

--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -1,5 +1,6 @@
 import { FC, useReducer } from 'react'
-import { UIContext, uiReducer } from './'
+import { UIContext } from './'
+import { uiReducer } from './uiReducer'
 
 export interface UIState {
   sidemenuOpen: boolean
