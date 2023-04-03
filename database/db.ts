1880f5f49a5e3f0cd2b8ae12db30bae1e264b324
nahuelcaceres <nahuelcaceres@users.noreply.github.com>
2023-04-02 23:04:27 -0300

    ref: always connect to mongodb

--- database/db.ts
+++ database/db.ts
@@ -11,31 +11,31 @@ const mongoConnection = {
 }
 
 export const connect = async() => {
-    if (mongoConnection.isConnected === 1) {
-      //   console.log('Ya estabamos conectados')
-        return
-    }
+   //  if (mongoConnection.isConnected === 1) {
+   //    //   console.log('Ya estabamos conectados')
+   //      return
+   //  }
 
-    if (mongoose.connections.length > 0 ){
-        mongoConnection.isConnected = mongoose.connections[0].readyState
+   //  if (mongoose.connections.length > 0 ){
+   //      mongoConnection.isConnected = mongoose.connections[0].readyState
 
-        if (mongoConnection.isConnected === 1){
-            // console.log('Usando conexion anterior')
-            return
-        }
+   //      if (mongoConnection.isConnected === 1){
+   //          // console.log('Usando conexion anterior')
+   //          return
+   //      }
 
-        await mongoose.disconnect()
-    }
+   //      await mongoose.disconnect()
+   //  }
     
-    await mongoose.connect(process.env.MONGO_URL || '')
+    await mongoose.connect(process.env.MONGO_URL)
     mongoConnection.isConnected = 1
 
    //  console.log('Conectado a MongoDB:', process.env.MONGO_URL)
 }
 
 export const disconnect = async() => {
-    if (process.env.NODE_ENV === 'development') return //en dev no me desconecto
-    if (mongoConnection.isConnected === 0 ) return   
+   //  if (process.env.NODE_ENV === 'development') return //en dev no me desconecto
+   //  if (mongoConnection.isConnected === 0 ) return   
 
     await mongoose.disconnect()
    //  console.log('Desconectado de MongoDB')
